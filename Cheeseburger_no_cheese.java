acf37d69629d80c1a0904e25a2c0dc6aac865e02
Raymond Le <118484385+Raymonldle@users.noreply.github.com>
2025-12-04 15:17:09 -0800

    2nd commit

--- Cheeseburger_no_cheese.java
+++
@@ -1,5 +0,0 @@
-public class Cheeseburger_no_cheese {
-  public static void main(String [] args) {
-    System.out.println("Hello Cheeseburgers");
-  }
-}
---
+++ Chezburger_no_chez.java
@@ -0,0 +1,5 @@
+public class Chezburger_no_chez {
+  public static void main(String [] args) {
+    System.out.println("Hello World");
+  }
+}
